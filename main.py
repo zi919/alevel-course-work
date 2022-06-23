3eb57efaa788fd8a9610f951421401c72f66c0d8
zi <97483792+zi919@users.noreply.github.com>
2022-06-23 10:48:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import sqlite3
+import hashFunctions
 conn=sqlite3.connect("database.db")
 c = conn.cursor()
 #c.execute("""CREATE TABLE USERS(username_field,password_field)""")
-def insert_emp(username,password):
+def insert(username,password):
+    username=hashFunctions.md5(username)
     with conn:
         c.execute("INSERT INTO USERS VALUES (?,?)", (username,password))
-def check_repeadted_username(username):
-    c.execute(f"SELECT * FROM USERS WHERE username_field= {username}")
-    if c.fetchall()==True:
+def check_repeated_username(username):
+    c.execute("SELECT * FROM USERS WHERE username_field=?",(username,))
+    if len(c.fetchall())>0:
         return True
     else:
-         return False
+        return False 
 
 def check_exsistance(username,password):
+    username=hashFunctions.md5(username)
 
-    c.execute(f"SELECT * FROM USERS WHERE username_field= {username} and password_field={password}  ")
+    c.execute(f"SELECT * FROM USERS WHERE username_field= ? and password_field=? ",(username,password,))
     return c.fetchall()
 
 
@@ -26,5 +29,6 @@ def update_password(username, password):
 def remove_record(username):
     with conn:
         c.execute(f"DELETE from USERS WHERE username_field={username}")
+
 
 
